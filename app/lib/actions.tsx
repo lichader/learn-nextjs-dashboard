3f9cd3360f59f26e9e58bcb98a5941f679aeeeec
lichader <1524470+lichader@users.noreply.github.com>
2025-06-01 12:38:39 +1000

    error handling

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -27,10 +27,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -46,17 +50,22 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices 
     set customer_id = ${customerId}, amount=${amountInCents}, status=${status} 
     where id = ${id}
   `;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error("Something went wrong with deleting invoice!");
   await sql`DELETE FROM invoices where id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
